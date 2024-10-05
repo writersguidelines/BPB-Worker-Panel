0c6370057b4b7bff6a1ad433933fd4ac664cedac
WGlines <123725614+writersguidelines@users.noreply.github.com>
2024-10-04 19:35:59 -0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,18 +7,18 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '95cefe4b-69f3-466a-ac56-6fc43b82b75c';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
-const proxyIPs= ['bpb.yousef.isegaro.com'];
+const proxyIPs= ['www.zsu.gov.ua','time.is','fbi.gov','129.146.137.89', '143.47.243.144', '152.70.155.147', '35.91.5.109', '52.25.64.223'];
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
-let dohURL = 'https://cloudflare-dns.com/dns-query';
-let trojanPassword = `bpb-trojan`;
+let dohURL = 'https://1.1.1.3/dns-query';
+let trojanPassword = `Gsd32ju6uI80hsMre`;
 // https://emn178.github.io/online-tools/sha224.html
 // https://www.atatus.com/tools/sha224-to-hash
-let hashPassword = 'b5d0a5f7ff7aac227bc68b55ae713131ffdf605ca0da52cce182d513';
+let hashPassword = 'bf2dcd57cce7cb906febce7aa0616a898de4b79540275e93c86abb28';
 let panelVersion = '2.6.4';
 
 if (!isValidUUID(userID)) throw new Error(`Invalid UUID: ${userID}`);
@@ -286,7 +286,7 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
+                         return new Response('Not found', { status: 404 });
                         url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
